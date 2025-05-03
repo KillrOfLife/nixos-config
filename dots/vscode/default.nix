cd30d0adc151d43100a7b4280668aacabc54dde5
killroflife <74971817+KillrOfLife@users.noreply.github.com>
2025-05-03 18:08:36 +0200

    bad extensions

--- dots/vscode/default.nix
+++ dots/vscode/default.nix
@@ -3,61 +3,49 @@
     enable = true;
     extensions = with pkgs.vscode-extensions; [
       # Official Nixpkgs extensions (add more if available in nixpkgs)
-          # Otherwise, use `vscode-marketplace` below for all others
-    vscode-marketplace."4ops".packer
-    vscode-marketplace."aaron-bond".better-comments
-    vscode-marketplace."akamud".vscode-theme-onedark
-    vscode-marketplace."albert".tabout
-    vscode-marketplace."alefragnani".project-manager
-    vscode-marketplace."arrterian".nix-env-selector
-    vscode-marketplace."bbenoist".nix
-    vscode-marketplace."britesnow".vscode-toggle-quotes
-    vscode-marketplace."catppuccin".catppuccin-vsc
-    vscode-marketplace."catppuccin".catppuccin-vsc-icons
-    vscode-marketplace."codezombiech".gitignore
-    vscode-marketplace."davidanson".vscode-markdownlint
-    vscode-marketplace."dev-kaustav".afterhours
-    vscode-marketplace."docker".docker
-    vscode-marketplace."dqisme".sync-scroll
-    vscode-marketplace."editorconfig".editorconfig
-    vscode-marketplace."fcrespo82".markdown-table-formatter
-    vscode-marketplace."geekbinarynet".vscode-arubaos-config-syntax
-    vscode-marketplace."github".copilot
-    vscode-marketplace."github".copilot-chat
-    vscode-marketplace."github".github-vscode-theme
-    vscode-marketplace."github".vscode-github-actions
-    vscode-marketplace."gitlab".gitlab-workflow
-    vscode-marketplace."gruntfuggly".todo-tree
-    vscode-marketplace."hashicorp".hcl
-    vscode-marketplace."hashicorp".terraform
-    vscode-marketplace."jnoortheen".nix-ide
-    vscode-marketplace."mechatroner".rainbow-csv
-    vscode-marketplace."meezilla".json
-    vscode-marketplace."mhutchie".git-graph
-    vscode-marketplace."michelemelluso".gitignore
-    vscode-marketplace."mikestead".dotenv
-    vscode-marketplace."mitchdenny".ecdc
-    vscode-marketplace."mkhl".direnv
-    vscode-marketplace."mohd-akram".vscode-html-format
-    vscode-marketplace."ms-azuretools".vscode-docker
-    vscode-marketplace."ms-kubernetes-tools".vscode-kubernetes-tools
-    vscode-marketplace."ms-python".debugpy
-    vscode-marketplace."ms-python".python
-    vscode-marketplace."ms-python".vscode-pylance
-    vscode-marketplace."ms-vscode-remote".remote-containers
-    vscode-marketplace."ms-vscode-remote".remote-ssh
-    vscode-marketplace."ms-vscode-remote".remote-ssh-edit
-    vscode-marketplace."ms-vscode-remote".remote-wsl
-    vscode-marketplace."ms-vscode-remote".vscode-remote-extensionpack
-    vscode-marketplace."ms-vscode".cpptools
-    vscode-marketplace."ms-vscode".makefile-tools
-    vscode-marketplace."ms-vscode".powershell
-    vscode-marketplace."ms-vscode".remote-explorer
-    vscode-marketplace."ms-vscode".remote-server
-    vscode-marketplace."owenfarrell".vscode-vault
-    vscode-marketplace."p1c2u".docker-compose
-    vscode-marketplace."peterjonsson".kickstart-language-support
-    vscode-marketplace."pinage404".nix-extension-pack
+      # Otherwise, use `vscode-marketplace` below for all others
+      # 4ops.packer
+      aaron-bond.better-comments
+      # albert.tabout
+      alefragnani.project-manager
+      arrterian.nix-env-selector
+      bbenoist.nix
+      # britesnow.vscode-toggle-quotes
+      catppuccin.catppuccin-vsc
+      catppuccin.catppuccin-vsc-icons
+      codezombiech.gitignore
+      davidanson.vscode-markdownlint
+      # docker.docker
+      # dqisme.sync-scroll
+      editorconfig.editorconfig
+      # fcrespo82.markdown-table-formatter
+      # geekbinarynet.vscode-arubaos-config-syntax
+      github.copilot
+      github.copilot-chat
+      github.vscode-github-actions
+      gruntfuggly.todo-tree
+      hashicorp.hcl
+      hashicorp.terraform
+      jnoortheen.nix-ide
+      mechatroner.rainbow-csv
+      # meezilla.json
+      mhutchie.git-graph
+      mikestead.dotenv
+      # mitchdenny.ecdc
+      mkhl.direnv
+      # mohd-akram.vscode-html-format
+      ms-azuretools.vscode-docker
+      ms-kubernetes-tools.vscode-kubernetes-tools
+      ms-vscode-remote.remote-containers
+      ms-vscode-remote.remote-ssh
+      ms-vscode-remote.remote-ssh-edit
+      ms-vscode-remote.vscode-remote-extensionpack
+      ms-vscode.remote-explorer
+      ms-vscode.remote-server
+      # owenfarrel.vscode-vault
+      # p1c2u.docker-compose
+      # peterjonsson.kickstart-language-support
+      # pinage404.nix-extension-pack
     ];
     userSettings = {
       "editor.suggestSelection" = "first";
