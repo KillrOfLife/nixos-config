27b93adc78780d819d8ccff4a68600cdfd124e79
killroflife <74971817+KillrOfLife@users.noreply.github.com>
2025-05-12 14:39:56 +0200

    framework

---
+++ machines/framework/default.nix
@@ -0,0 +1,70 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    ./disko.nix
+    ./boot.nix
+    ./framework_patches.nix
+    ../../modules/hyprland
+  ];
+
+  environment.systemPackages = [
+    pkgs.s-tui
+    pkgs.stress
+    pkgs.firefox-bin
+  ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-partlabel/disk-main-root";
+    fsType = "ext4";
+  };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-partlabel/disk-main-ESP";
+    fsType = "vfat";
+  };
+
+  hardware = {
+    bluetooth = {
+	    enable = true;
+	    powerOnBoot = true;
+	    settings = {
+		    General = {
+		      Enable = "Source,Sink,Media,Socket";
+		      Experimental = true;
+		    };
+      };
+    };
+    enableRedistributableFirmware = true;
+    # cpu.intel = {
+    #   updateMicrocode = true;
+    #   # ryzen-smu.enable = true;
+    # };
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    # opengl.enable = true;
+    # nvidia = {
+    #   modesetting.enable = true;
+    #   powerManagement.enable = true;
+    # };
+  };
+
+  services = {
+    openssh.enable = true;
+    blueman.enable = true;
+    gnome.gnome-keyring.enable = true;
+
+    fstrim.enable = lib.mkDefault true; # enable fstrim for SSDs
+  };
+
+  networking = {
+    networkmanager.enable = true;
+    hostName = "dell";
+  };
+}
